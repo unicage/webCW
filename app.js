a1167fcb5699739565a6c2d4a1b40e85f5c8d435
unicage <ulugbekghostbatory@gmail.com>
2021-04-06 23:11:46 +0500

    My first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'pug')
+
+// local host 8000
+app.get('/', (re, res) => {
+    res.render('home')
+
+})
+
+
+app.listen(PORT, (err) => {
+    if (err) throw err
+
+    console.log('The app started its work on ${PORT}')
+})
